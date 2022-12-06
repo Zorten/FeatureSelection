3a6ac9c44b5e0a4ff746b91f0d4d10003449d9a2
Zorten <zergionaruto@hotmail.com>
2022-12-06 00:40:30 -0800

    cross validation set up finished

--- featureSelection.py
+++ featureSelection.py
@@ -1,18 +1,40 @@
 import pandas as pd
+import math
 import random
 
 data = pd.read_csv("smallTest.txt", sep="  ", header=None, engine='python')
 #print(data.head(10))
 
 #Cross Validation
-def leave_one_out_cross_validation(data): #, current_set, feature_to_add):
+def leave_one_out_cross_validation(data, current_set, feature_to_add):
+    number_correctly_classified = 0
     #Loop to traverse the instances
     for i in range(0, len(data)):
+        #Initialize variables
         object_to_classify = data.iloc[i, 1:]
         label_object_to_classify = data.iloc[i, 0]
+        nearest_neighbor_distance = float('inf')
+        nearest_neighbor_location = float('inf')
 
-        print("Looping over i, at the " + str(i+1) + " location")
-        print("The " + str(i+1) + "th object is in class " + str(label_object_to_classify))
+        #Loop to compare each instance with all its neighbors
+        for k in range(0, len(data)):
+            #Don't compare to yourself
+            if (k != i):
+                #print("Ask if " + str(i+1) + " is nearest neighbor with " + str(k+1))
+                distance = math.dist(object_to_classify, data.iloc[k, 1:]) #math.sqrt(sum(object_to_classify - data.iloc[k, 1:])**2)
+                if (distance < nearest_neighbor_distance):
+                    nearest_neighbor_distance = distance
+                    nearest_neighbor_location = k
+                    nearest_neighbor_label = data.iloc[nearest_neighbor_location, 0]
+
+        # print("Object " + str(i+1) + " is class " + str(label_object_to_classify))
+        # print("-- Its nearest neighbor is " + str(nearest_neighbor_location + 1) + " which is in class " + str(nearest_neighbor_label))
+
+        if (label_object_to_classify == nearest_neighbor_label):
+            number_correctly_classified += 1
+
+    accuracy = number_correctly_classified / len(data)
+    return accuracy
 
 
 #Search function
@@ -45,9 +67,12 @@ def feature_search(data):
 
         #Update working set of features
         current_set_of_features.append(feature_to_add_at_this_level)
-        print("On level " + str(i) + " I added feature " + str(feature_to_add_at_this_level) + " to current set")
+        print("On level " + str(i) + " I added feature " + str(feature_to_add_at_this_level) + " to current set, given an accuracy of " + str(best_so_far_accuracy))
 
     print(current_set_of_features)
 
-leave_one_out_cross_validation(data)  
+# accuracy = leave_one_out_cross_validation(data)  
+# print("Accuracy calculated: " + str(accuracy))
+
+feature_search(data)
 
